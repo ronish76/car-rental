dc6a6a4b1d3651072f39e7184098ef734ae85a41
ronish <ronish.magar76@gmail.com>
2024-02-29 22:08:48 +0545

    pages and css added

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,53 @@
-import React,{Fragment} from 'react'
+// import React, { Fragment } from "react";
+// import Header from "../Header/Header";
+// import Footer from "../Footer/Footer";
+// import Routers from "../../routers/Routers";
 
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import Routers from '../../routers/Routers'
+// const Layout = () => {
+//   return (
+//     <Fragment>
+//       <Header />
+//       <div>
+//         <Routers />
+//       </div>
+//       <Footer />
+//     </Fragment>
+//   );
+// };
+
+// export default Layout;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { Fragment } from 'react'
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Routers from '../../routers/Routers';
 const Layout = () => {
   return <Fragment>
     <Header/>
-        <div>
-            <Routers/>
-        </div>
+    <div>
+    <Routers/>
+    </div>
 
     <Footer/>
+
+    
+
 
   </Fragment>
 }
